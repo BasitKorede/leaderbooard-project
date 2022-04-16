4f146522898be8a2b81e58a1cc70cf02a51a1197
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-04-16 11:45:58 +0100

    Add addtional styling to butify psge

--- js/modules/dom.js
+++ js/modules/dom.js
@@ -1,8 +1,7 @@
-/* eslint-disable import/no-cycle */
-import fetchData from '../fuctionalties.js';
-
 const API_KEY = 's5I1Iezs2fp5U1vv55v4';
 export const URI = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${API_KEY}/scores`;
+
+const resultUl = document.querySelector('.board-list');
 export const displayScores = (list, res) => {
   const { result } = res;
   const scores = [];
@@ -11,12 +10,46 @@ export const displayScores = (list, res) => {
   }
   list.innerHTML = '';
   scores.forEach((score) => {
-    list.innerHTML += `<li>${score[0]}: ${score[1]}</li>`;
+    list.innerHTML += `<li class='board-list-item'>${score[0]}: ${score[1]}</li>`;
   });
   if (scores.length > 5) { list.classList.add('scroll'); }
 };
 
+const fetchData = async () => {
+  await fetch(URI, {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((result) => displayScores(resultUl, result));
+};
+
 const refresh = document.querySelector('.refresh-button');
 refresh.addEventListener('click', () => {
+  fetchData();
+});
+export const postData = async (user, score) => {
+  await fetch(URI, {
+    method: 'POST',
+    body: JSON.stringify({ user, score }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json);
+};
+
+export const sendData = async () => {
+  const submitScore = document.querySelector('#form');
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
+  submitScore.addEventListener('submit', (e) => {
+    e.preventDefault();
+    postData(nameInput.value, scoreInput.value);
+    submitScore.reset();
+  });
+};
+document.addEventListener('DOMContentLoaded', () => {
+  sendData();
   fetchData();
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// eslint-disable-next-line no-unused-vars
-import { URI, displayScores } from '../js/modules/dom.js';
-
-import sendData from '../js/modules/refresh.js';
-import postData from '../js/fuctionalties.js';
+/* eslint-disable no-unused-vars */
+import {
+  URI, displayScores, postData, sendData,
+} from '../js/modules/dom.js';
 
 displayScores();
 
